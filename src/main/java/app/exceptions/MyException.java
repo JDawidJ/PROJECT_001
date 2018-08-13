aee290a019df6191ffbaa29b0c6c18f58788b0f5
Jerzy Jakubowski <j.d.jakubowski@wp.pl>
2018-08-13 20:15:07 +0200

    Add myException

---
+++ src/main/java/app/exceptions/MyException.java
@@ -0,0 +1,16 @@
+package app.exceptions;
+
+import lombok.AllArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@AllArgsConstructor
+public class MyException extends RuntimeException {
+    private String exceptionMessage;
+    private LocalDateTime exceptionDateTime;
+
+    @Override
+    public String getMessage() {
+        return "[ " + exceptionDateTime + " ]" + exceptionMessage;
+    }
+}
